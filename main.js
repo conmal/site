b0728e345d7ffcc1628afc15f1d00ec5df1b039d
conmal <conmaltranslations@mail.com>
2021-04-20 12:22:01 +0300

    Update

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("client id:", CLIENT_ID);
+const client_id = ${{secret.CLIENT_ID}};
+
+console.log("client id:", client_id);
+
+(function($){
+    var albumID = '38xVNmG';
+    var albumAPI = "https://api.imgur.com/3/album/" + albumID + "/images";
+
+	$.ajax({
+		url: albumAPI,
+		headers:{
+			'Authorization':'Client-ID ' + client_id
+		},
+		type: 'GET',
+		dataType: 'json',
+		success: function(data) {
+			alert(data.data[0].link);
+		},
+		error: function() { console.log("ERRORZ"); }
+	});
+})(jQuery);
