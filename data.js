6c01e687d85545d546dd01b36ab5c30041164866
conmal <conmaltranslations@mail.com>
2021-04-22 14:00:40 +0300

    Update

--- data.js
+++ data.js
@@ -1,14 +1,3 @@
-var CHAPTER_LINKS = {
-	gfEOOXL: "10",
-	Rf7T2Id: "9",
-	Bsj2jcH: "7",
-	M9Zx73G: "6",
-	Bm8A1WZ: "4"
-}
-
-CHAPTER_LINKS['0g3DQpW'] = "8";
-CHAPTER_LINKS['38xVNmG'] = "5";
-
 var SERIES = {};
 
 var gou = {};
