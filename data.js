5d5b27d3ea162a4737e3046ec0ec6ad633e559c4
conmal <conmaltranslations@mail.com>
2021-04-24 18:31:54 +0300

    Update

--- data.js
+++ data.js
@@ -8,6 +8,7 @@ gou["7"] = "Bsj2jcH";
 gou["6"] = "M9Zx73G";
 gou["5"] = "38xVNmG";
 gou["4"] = "Bm8A1WZ";
+gou["name"] = "Higurashi Gou";
 SERIES["gou"] = gou;
 
 const CLIENT_ID = "c45bf8e97fb563f";
